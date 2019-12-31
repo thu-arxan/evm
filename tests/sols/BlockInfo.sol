97b8768773684e4b59725815d6e79c835e00d260
junqi <huang-jq18@mails.tsinghua.edu.cn>
2019-12-31 14:53:55 +0800

    add blockinfo test

---
+++ tests/sols/BlockInfo.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract SimpleBlock {
+    function getAddress() public view returns (address) {
+        return address(this);
+    }
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    function getOrigin() public view returns (address) {
+        return tx.origin;
+    }
+    function getGasprice() public view returns (uint) {
+        return tx.gasprice;
+    }
+    function getCoinbase() public view returns (address) {
+        return block.coinbase;
+    }
+    function getTimestamp() public view returns (uint) {
+        return block.timestamp;
+
+    }
+    function getNumber() public view returns (uint) {
+        return block.number;
+    }
+    function getDifficulty() public view returns (uint) {
+        return block.difficulty;
+    }
+    function getGaslimit() public view returns (uint) {
+        return block.gaslimit;
+    }
+}
