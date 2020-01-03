57974bb073250afa1e849895c759891dcb7dc533
liuyihua <i@liuyihua.com>
2020-01-03 15:04:06 +0800

    update code comments

--- eabi/address.go
+++ eabi/address.go
@@ -11,23 +11,6 @@ var (
 	addressToString func([]byte) string
 )
 
-// SetAddressParser set address length and toString function
-// If length > 20, something may be lost while running
-// func SetAddressParser(length int, toString func([]byte) string) error {
-// 	if length <= 0 || length > 20 {
-// 		return errors.New("length should belong to (0,20]")
-// 	}
-// 	addressLength = length
-// 	addressToString = toString
-// 	return nil
-// }
-
-// ResetAddressParser reset to default
-// func ResetAddressParser() {
-// 	addressLength = 20
-// 	addressToString = nil
-// }
-
 // SetAddressParser set address reflect type and its length
 func SetAddressParser(t reflect.Type, length int) {
 	addressT = t
