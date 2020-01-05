0f47c86ff8ff270393de6d8fd6696cafe1e19746
liuyihua <i@liuyihua.com>
2020-01-05 16:31:48 +0800

    update log struct

--- log.go
+++ log.go
@@ -1,12 +1,26 @@
 package evm
 
-import "evm/core"
+import (
+	"evm/core"
+)
 
 // Log is the log of evm
 type Log struct {
-	Address Address
+	// Consensus field
+	Address Address `json:"address"`
 	// list of topics provided by the contract.
-	Topics []core.Word256
+	Topics []core.Word256 `json:"topics"`
 	// supplied by the contract, usually ABI-encoded
-	Data []byte
+	Data []byte `json:"data"`
+
+	// Derived fields, so the database should record the context to support these fields
+	BlockNumber uint64 `json:"blockNumber"`
+	// hash of the transaction
+	TxHash []byte `json:"transactionHash"`
+	// index of the transaction in the block
+	TxIndex uint `json:"transactionIndex"`
+	// hash of the block in which the transaction was included
+	BlockHash []byte `json:"blockHash"`
+	// index of the log in the block
+	Index uint `json:"logIndex"`
 }
