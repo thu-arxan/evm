86f9125dfd4a7925a3376f3c6bf7e977b19a1acf
junqi <huang-jq18@mails.tsinghua.edu.cn>
2020-01-13 17:31:04 +0800

    debug

--- tests/create_sol_test.go
+++ tests/create_sol_test.go
@@ -25,8 +25,8 @@ func TestCreateSol(t *testing.T) {
 	memoryDB := db.NewMemory(bc.NewAccount)
 	var origin = example.HexToAddress("6ac7ea33f8831ea9dcc53393aaa88b25a785dbf0")
 	var exceptAddress = `cd234a471b72ba2f1ccf0a70fcaba648a5eecd8d`
-	CCode, CAddress = deployContract(t, memoryDB, bc, origin, binBytes, exceptAddress, "", 0)
-	callCreate(t, memoryDB, bc, origin, mustPack(createAbi, "createAndGetBalance", "44", "0"), 84357)
+	CCode, CAddress = deployContract(t, memoryDB, bc, origin, binBytes, exceptAddress, "", 344952)
+	callCreate(t, memoryDB, bc, origin, mustPack(createAbi, "createAndGetBalance", "44", "0"), 95396)
 }
 
 func callCreate(t *testing.T, db evm.DB, bc evm.Blockchain, caller evm.Address, payload []byte, gasCost uint64) {
